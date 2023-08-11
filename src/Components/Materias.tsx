1b57e2bb7e5ed6ba1e8278aa1b22b8c25a7c2a02
Federico Robledo <federicorobledo85@gmail.com>
2023-08-11 19:49:16 -0300

    add firebase cli

--- src/Components/Materias.tsx
+++ src/Components/Materias.tsx
@@ -3,71 +3,83 @@ import FamilyRestroomIcon from "@mui/icons-material/FamilyRestroom";
 import BalanceIcon from "@mui/icons-material/Balance";
 import ReactWhatsapp from "react-whatsapp";
 import { Center } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
 export function Materias() {
   return (
-    <div className="flex flex-col md:flex-row md:justify-around">
-      <h2>
-        En nuestro estudio juridico nos especializamos en las siguientes areas
-      </h2>
-      <div className="mb-24">
-        <FactoryIcon color="secondary" sx={{ fontSize: 100 }} />
-        <ul>
-          <li>Despidos</li>
-          <li>Indemnizaciones</li>
-          <li>Telegramas laborales</li>
-        </ul>
-      </div>
-      <div className="mb-24">
-        <FamilyRestroomIcon sx={{ fontSize: 100 }} />
-        <ul>
-          <li>Divorcios</li>
-          <li>Alimentos</li>
-          <li>Regimen de comunicacion</li>
-        </ul>
-      </div>
-      <div className=" mb-32">
-        <BalanceIcon sx={{ fontSize: 100 }} />
-        <ul>
-          <li>Daños y perjuicios</li>
-          <li>Redaccion de contratos</li>
-          <li>Sucesiones</li>
-          <li>Derecho del consumidor</li>
-        </ul>
-      </div>
-      <h3 className=" ml-1 mb-10 lg:hidden">Consultenos</h3>
-      <div className=" ml-32 sm:mr-32 lg:hidden">
-        {/* variant="gradient"
+    <motion.div
+      animate={{ x: 100 }}
+      transition={{ ease: "easeOut", duration: 2 }}
+    >
+      <div className="flex flex-col md:flex-row md:justify-around">
+        <h2>Poseemos amplia experiencia en diversas ramas del derecho como:</h2>
+        <div className="mb-24">
+          <FactoryIcon color="secondary" sx={{ fontSize: 100 }} />
+          <ul>
+            <button className="  text-white text-lg bg-blue-700 rounded-lg p-2">
+              Derecho laboral
+            </button>
+            <li>Despidos</li>
+            <li>Indemnizaciones</li>
+            <li>Telegramas laborales</li>
+          </ul>
+        </div>
+        <div className="mb-24">
+          <FamilyRestroomIcon sx={{ fontSize: 100 }} />
+          <h2 className=" text-lg">Derecho de familia</h2>
+          <ul>
+            <button className="  text-white text-lg bg-blue-700 rounded-lg p-2">
+              Derecho laboral
+            </button>
+            <li>Divorcios</li>
+            <li>Alimentos</li>
+            <li>Regimen de comunicacion</li>
+          </ul>
+        </div>
+        <div className=" mb-32">
+          <BalanceIcon sx={{ fontSize: 100 }} />
+          <h2 className=" text-lg">Derecho civil</h2>
+          <ul>
+            <li>Daños y perjuicios</li>
+            <li>Redaccion de contratos</li>
+            <li>Sucesiones</li>
+            <li>Derecho del consumidor</li>
+          </ul>
+        </div>
+        <h3 className=" ml-1 mb-10 lg:hidden">Consultenos</h3>
+        <div className=" ml-32 sm:mr-32 lg:hidden">
+          {/* variant="gradient"
         size="sm"  */}
 
-        <Center
-          w="50px"
-          h="50px"
-          bg="#25D366"
-          color="white"
-          mx="auto"
-          borderRadius={"3xl"}
-        >
-          <ReactWhatsapp number="+5492235044778" element="button" message="">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="icon icon-tabler icon-tabler-brand-whatsapp"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              stroke="white"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9"></path>
-              <path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1"></path>
-            </svg>
-          </ReactWhatsapp>
-        </Center>
+          <Center
+            w="50px"
+            h="50px"
+            bg="#25D366"
+            color="white"
+            mx="auto"
+            borderRadius={"3xl"}
+          >
+            <ReactWhatsapp number="+5492235044778" element="button" message="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-brand-whatsapp"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="white"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9"></path>
+                <path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1"></path>
+              </svg>
+            </ReactWhatsapp>
+          </Center>
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
